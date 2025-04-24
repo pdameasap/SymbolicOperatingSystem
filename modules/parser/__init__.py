b3a586cd1dcec27ae301fc23aefb490b78032e56
Marhysa Black <pdameasap@gmail.com>
2025-04-24 18:38:52 -0500

    Update __init__.py
    Signed-off-by: Marhysa Black <pdameasap@gmail.com>

--- modules/parser/__init__.py
+++ modules/parser/__init__.py
@@ -1,2 +1,4 @@
 from .SemanticParserCore import SemanticParserCore
 from .SemanticParser import SemanticParser
+from .symbolic_normalizer import normalize_noun
+from .symbolic_nouns import SYMBOLIC_NOUNS
