63a91ea33b997bd0e8db2ba809510f392b875e01
Marhysa Black <pdameasap@gmail.com>
2025-04-24 19:09:31 -0500

    Update CorpusParser.py
    Signed-off-by: Marhysa Black <pdameasap@gmail.com>

--- modules/parser/CorpusParser.py
+++ modules/parser/CorpusParser.py
@@ -2,7 +2,7 @@ import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parents[1]))
 
-from modules import CorpusParser
+from modules.parser.CorpusParser import CorpusParser
 
 document = [
     "I am in need of a programmer and a grammarian.",
