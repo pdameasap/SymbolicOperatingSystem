74858df56c8c3e330659e8f93d48beaf28aeb448
Marhysa Black <pdameasap@gmail.com>
2025-04-28 14:45:23 -0500

    Update SemanticParser.py
    Signed-off-by: Marhysa Black <pdameasap@gmail.com>

--- modules/parser/SemanticParser.py
+++ modules/parser/SemanticParser.py
@@ -1,8 +1,8 @@
 # File: modules/parser/SemanticParser.py
 
 import re
-from modules.parser.symbolic_nouns import SYMBOLIC_NOUNS
-from modules.parser.symbolic_normalizer import normalize_noun
+from SymbolicOperatingSystem.modules.parser.symbolic_nouns import SYMBOLIC_NOUNS
+from SymbolicOperatingSystem.modules.parser.symbolic_normalizer import normalize_noun
 
 class SemanticParser:
 
