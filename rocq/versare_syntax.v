e64ff6bc8b2b13f69691880d06d69a3bcf532eae
Marhysa Black <pdameasap@gmail.com>
2025-05-05 11:41:48 -0500

    Create versare_syntax.v
    Signed-off-by: Marhysa Black <pdameasap@gmail.com>

---
+++ rocq/versare_syntax.v
@@ -0,0 +1,40 @@
+(* versare_syntax.v *)
+
+Require Import Coq.Strings.String.
+Require Import Coq.Lists.List.
+Import ListNotations.
+
+Module VersareSyntax.
+
+(* === Emoji inventory === *)
+Inductive emoji : Type :=
+  | EmojiLove
+  | EmojiSad
+  (* TODO: extend with your full emoji set *)
+  .
+
+(* === Z‑Glyph Primes === *)
+Inductive Zglyph : Type :=
+  | Z1  | Z2  | Z3  | Z4  | Z5  | Z6  | Z7  | Z8  | Z9
+  | Z10 | Z11 | Z12 | Z13 | Z14 | Z15 | Z16 | Z17 | Z18.
+
+(* === Noun lexicon === *)
+Inductive noun : Type :=
+  (* TODO: replace these stubs with your ~200 nouns *)
+  | N_Ocean
+  | N_Heart
+  .
+
+(* === Core AST === *)
+Inductive expr : Type :=
+  | EClause  (subj : noun) (pred : list Zglyph) (mod : option emoji)
+  | EDefine  (name : string) (body : expr)
+  | EInvoke  (name : string)
+  | EIf      (cond then_br : expr)
+  | EEval    (z : Zglyph) (body : expr)
+  | ECall    (fname : string) (args : list expr)
+  .
+
+End VersareSyntax.
+
+Export VersareSyntax.
